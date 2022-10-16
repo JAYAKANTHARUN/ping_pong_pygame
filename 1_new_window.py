5469d74a130ff0a8f8475e65e219b7752a98b968
Jayakanth Arun <jayakantharun03@gmail.com>
2022-10-16 19:39:29 +0530

    Updated Comments

--- 1_new_window.py
+++ 1_new_window.py
@@ -1,15 +1,19 @@
+#import pygame library
 import pygame,sys
 
 pygame.init()
 
 clock=pygame.time.Clock()
 
+#creating new window
 screenwidth=1000
 screenheight=700
 screen=pygame.display.set_mode((screenwidth,screenheight)) 
 
+#name for window
 name=pygame.display.set_caption("Ping Pong Game")
 
+#run the window
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
