e0a350658eacb86f192c57d6e95904818daa6c8d
Jayakanth Arun <jayakantharun03@gmail.com>
2022-10-17 21:27:00 +0530

    created shapes

--- main.py
+++ main.py
@@ -17,6 +17,18 @@ name=pygame.display.set_caption("Ping Pong Game")
 icon=pygame.image.load("image.jpg")
 pygame.display.set_icon(icon)
 
+#colors
+bgcolor=(255,255,255)
+ballcolor=(255,165,0)
+player1color=(255,0,0)
+player2color=(0,0,0)
+linecolor=(0,0,0)
+
+#creating shapes
+ball=pygame.Rect(screenwidth/2-15,screenheight/2-15,30,30)
+player1=pygame.Rect(10, screenheight / 2 - 70, 10,120)
+player2=pygame.Rect(screenwidth - 20, screenheight / 2 - 70, 10,120)
+
 #run the window
 while True:
     for event in pygame.event.get():
