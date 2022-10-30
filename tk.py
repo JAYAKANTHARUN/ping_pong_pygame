670a9c412843ade86aa7150316eea62f90c6413e
Jayakanth Arun <jayakantharun03@gmail.com>
2022-10-30 19:09:51 +0530

    Update tk.py

--- tk.py
+++ tk.py
@@ -18,7 +18,16 @@ img=ImageTk.PhotoImage(Image.open("imagenew2.jpg"))
 imagelabel=Label(root,image=img)
 imagelabel.pack()
 
-label2=Label(root,text="About the game :-")
+label2=Label(root,text="\n\tABOUT THE GAME :-",font=("algerian",20),bg="black",fg="orange")
+label3=Label(root,text="\t-> THIS IS A BASIC PING PONG GAME",font=("algerian",20),bg="black",fg="orange")
+label4=Label(root,text="\t-> ITS A 1VS1 GAME",font=("algerian",20),bg="black",fg="orange")
+label5=Label(root,text="\t-> FIRST PLAYER TO SCORE 5 POINTS WINS",font=("algerian",20),bg="black",fg="orange")
+label6=Label(root,text="\t-> A RELAX TIME OF 3 SECONDS WILL BE GIVEN AT THE START AND ALSO AFTER EACH POINT",font=("algerian",20),bg="black",fg="orange")
 
+label2.pack(anchor="w")
+label3.pack(anchor="w")
+label4.pack(anchor="w")
+label5.pack(anchor="w")
+label6.pack(anchor="w")
 
 root.mainloop()
